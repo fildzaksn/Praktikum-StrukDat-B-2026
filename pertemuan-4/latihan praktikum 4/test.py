471878068c1163bd814f5755682273c31556bb35
fildzaksn <fildza.khusaini4589@student.unri.ac.id>
2026-02-24 12:57:49 +0700

    mengumpulkan file latihan praktikum 4

---
+++ pertemuan-4/latihan praktikum 4/test.py
@@ -0,0 +1,8 @@
+#test.py
+from colorama import Fore, Back, Style, init
+init()
+
+print(Fore.RED + "Teks Merah")
+print(Fore.GREEN + "Teks Hijau")
+print(Fore.BLUE + Back.YELLOW + "Teks Biru, Bg kuning")
+print(Style.RESET_ALL + "Kembali Normal")
